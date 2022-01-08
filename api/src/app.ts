cac7c28db890543f236f4f75f0d5ec93da1096a8
Sam Hirst <45145746+Naeviant@users.noreply.github.com>
2022-01-08 18:21:31 +0000

    automatically get list of roads every 24 hours

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,6 +1,35 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import cron from 'node-cron';
+import axios, { Axios, AxiosResponse } from 'axios';
+import fs, { rmSync } from 'fs';
+
+import RoadList from './types/RoadList';
+
+// Update list of motorways - every 24 hours at 12pm
+cron.schedule('* 12 * * *', async () => {
+    console.log('Fetching List of Roads...');
+    
+    const resp = await axios.get('https://www.trafficengland.com/api/network/getMotorwayJunctions');
+    const data = resp.data as RoadList;
+
+    const roads = Object.keys(data);
+
+    fs.writeFileSync(__dirname + '/../data/roads.json', JSON.stringify(roads));
+    
+    console.log('Finished Fetching List of Roads')
+});
 
 const app = express();
+
+app.get('/', (req: Request, res: Response) => {
+    res.send({ status: 200, data: [] });
+});
+
+app.get('/roads', async (req: Request, res: Response) => {
+    const roads = fs.readFileSync(__dirname + '/../data/roads.json', 'utf8');
+
+    res.send({ status: 200, data: JSON.parse(roads) });
+});
 
 app.listen(8080, () => {
     console.log('Server Listening');
---
+++ api/src/types/RoadList.ts
@@ -0,0 +1,3 @@
+export default interface RoadList {
+    [key: string]: [{}]
+};
